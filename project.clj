92b3b55b416fb8086ccb4aa51d786e7cd4933d50
vutr <me@vutr.io>
2020-08-23 15:18:38 +0700

    update

--- project.clj
+++ project.clj
@@ -2,7 +2,16 @@
   :description "Main backend for KA"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
-  :main ^:skip-aot ka-backend.core
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]
+                 [metosin/reitit "0.4.2"]
+                 [ring/ring-devel "1.8.0"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [aero "1.1.6"]]
+  :main ka-backend.core
+  :repl-options {:init-ns ka-backend.core}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :user
+             {:plugins [[lein-kibit "0.1.8"]
+                        [jonase/eastwood "0.3.10"]]}})

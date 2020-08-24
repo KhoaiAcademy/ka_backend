c85d522dc9e89371d88cd3ed366d74281893f7c2
vutr <me@vutr.io>
2020-08-24 21:52:52 +0700

    Add hugsql

--- project.clj
+++ project.clj
@@ -8,6 +8,9 @@
                  [ring/ring-devel "1.8.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [clj-postgresql "0.7.0"]
+                 [seancorfield/next.jdbc "1.1.582"]
+                 [org.postgresql/postgresql "42.2.2"]
+                 [com.layerware/hugsql "0.5.1"]
                  [aero "1.1.6"]]
   :main ka_backend.core
   :repl-options {:init-ns ka_backend.core}
